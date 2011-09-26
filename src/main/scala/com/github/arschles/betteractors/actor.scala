73db1fc46321747ed123dc839daf0e860f1581d0
Aaron Schlesinger <arschles@gmail.com>
2011-09-26 00:31:55 -0700

    feeble attempt at implementing in-serial execution

--- src/main/scala/com/github/arschles/betteractors/actor.scala
+++ src/main/scala/com/github/arschles/betteractors/actor.scala
@@ -1,6 +1,7 @@
 package com.github.arschles.betteractors
 
-import java.util.concurrent.{CountDownLatch, LinkedBlockingQueue}
+import java.util.concurrent.{DelayQueue, Delayed, Exchanger, TimeUnit, TimeoutException}
+import java.lang.Thread
 
 trait Operation[In, Out] {
 	def apply(in:In):Out
@@ -8,37 +9,54 @@ trait Operation[In, Out] {
 
 sealed class Actor[In, Out](f:Operation[In, Out]) {
   import Actor._
-	import Strategies._
+  import Strategies.Synchronous
   import Future._
+
+  //the synchronous strategy for futures means that we'll waste no background threads just waiting on the Exchanger
+  implicit private val strategy = new Synchronous
+
+  private case class DelayedPayload(input:In, delayMS:Long) extends Delayed {
+    val exchanger = new Exchanger[Option[Out]]
+    def getDelay(unit:TimeUnit) = unit.convert(delayMS, TimeUnit.MILLISECONDS)
+    def compareTo(delayed:Delayed) = getDelay(TimeUnit.MILLISECONDS).compareTo(delayed.getDelay(TimeUnit.MILLISECONDS))
+  }
+
+  private val queue = new DelayQueue[DelayedPayload]
+
+  private val thread = new Thread {
+    override def run() {
+      val payload = queue.take
+      payload.exchanger.exchange(Some(f(payload.input)), 1, TimeUnit.SECONDS)
+      run()
+    }
+  }
+  thread.start()
 
 	/**
 	* equivalent to send(a, 0)
 	*/
 	def ! (a:In): Future[Out] = send(a, 0)
-	
+
 	/**
 	* send an input to this actor, delay doing any operation on the input for a given amount of time
 	* and return a future representing the result. the future will block on get for at least delayMS milliseconds
 	* plus the execution time of the operation that this actor contains
 	*/
 	def send(a:In, delayMS:Long = 0): Future[Out] = {
-		implicit val strategy = new Delayed(delayMS)
-    lazy val op = f(a)
-    future(op)
-	}
-	
-	/**
-	* send an input to this actor and call countDown on the given CountDownLatch when the operation on this actor is complete
-	*/
-	def send(a:In, latch:CountDownLatch) {
-		implicit val strategy:Strategy = new ExecutorService
+    val payload = new DelayedPayload(a, delayMS)
     lazy val op = {
-			f(a)
-			latch.countDown()
-		}
+      try {
+        payload.exchanger.exchange(None, 1, TimeUnit.SECONDS).get
+      }
+      catch {
+        case e:TimeoutException =>
+          throw new TimeoutException("failed to get result for input " + a + " after 1 second (" + e.getMessage + ")")
+      }
+    }
+    queue.put(payload)
     future(op)
 	}
-	
+
 	//compose this actor with a new actor. return an actor which forwards the result of this to actor2
 	def |||[Out2](actor2:Actor[Out, Out2]):Actor[In, Out2] = actor { in =>
     ((this ! in) ||| (data => actor2 ! data)).get
